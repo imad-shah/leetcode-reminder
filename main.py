436613d7126f32208fe0316dca35a30747f90bac
imad-shah <ishah5@student.gsu.edu>
2026-01-04 23:54:20 -0500

    feature: normalized inputs, ordered problems by due date, & gave clear response formatting for submissions

--- main.py
+++ main.py
@@ -16,11 +16,28 @@ def calculate_due_date(difficulty: int) -> datetime:
     days = {1: 14, 2: 7, 3: 5, 4: 2, 5: 1,}[difficulty]
     return (datetime.today() + timedelta(days=days)).replace(microsecond=0)
 
+def ordinal(day):
+    suffix = ''
+    if day == 11 or day == 12 or day == 13:
+        suffix += 'th'
+    else:
+        last_digit = day % 10
+        match (last_digit):
+            case 1:
+                suffix += 'st' 
+            case 2:
+                suffix += 'nd'
+            case 3:
+                suffix += 'rd'
+            case _:
+                suffix += 'th'
+    return f'{day}{suffix}'
+
 @app.post('/submit')
 async def submit(submission: Submission, db: Session = Depends(get_db)):
     submitted_at = datetime.today().replace(microsecond=0)
     due = calculate_due_date(submission.difficulty)
-    existing = db.query(SubmissionModel).filter(SubmissionModel.problem == submission.problem).first()
+    existing = db.query(SubmissionModel).filter(SubmissionModel.problem == submission.problem.lower().strip()).first()
 
     if existing:
         existing.difficulty = submission.difficulty # type: ignore
@@ -29,14 +46,14 @@ async def submit(submission: Submission, db: Session = Depends(get_db)):
 
     else:
         db_submission = SubmissionModel(
-            problem=submission.problem,
+            problem=submission.problem.lower().strip(),
             difficulty=submission.difficulty,
             submitted_at=submitted_at,
             due=due
         )
         db.add(db_submission)
     db.commit()
-    return submission
+    return f'Problem {submission.problem.title()} solved. Due next on {due.strftime("%B")} {ordinal(int(due.strftime('%-d')))}'
 
 @app.get('/problems')
 async def get_problems(db: Session = Depends(get_db)):
@@ -44,5 +61,5 @@ async def get_problems(db: Session = Depends(get_db)):
 
 @app.get('/due')
 async def due_problems(limit: int = Query(default=5, ge=1, le=10), db: Session = Depends(get_db)):
-    due = db.query(SubmissionModel).filter(SubmissionModel.due <= datetime.today()).all()
-    return due[:limit]
+    due = db.query(SubmissionModel).filter(SubmissionModel.due <= datetime.today()).order_by(SubmissionModel.due.asc()).limit(limit).all()
+    return due
